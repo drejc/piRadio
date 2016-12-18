aeb3c487883042f0e6b2377d191b10ccd9636734
drejc <drey.ray@gmail.com>
2016-12-18 09:34:27 -0500

    Added simple touchscreen test

---
+++ test/touchscreen_test.py
@@ -0,0 +1,29 @@
+import sys, pygame
+from pygame.locals import *
+import time
+import os
+
+os.environ["SDL_FBDEV"] = "/dev/fb1"
+os.environ["SDL_MOUSEDEV"] = "/dev/input/touchscreen"
+os.environ["SDL_MOUSEDRV"] = "TSLIB"
+pygame.init()
+
+def main():
+        while True:
+                for event in pygame.event.get():
+                        if event.type == pygame.MOUSEBUTTONDOWN:
+                                print("screen pressed") #for debugging purposes
+                                pos = (pygame.mouse.get_pos() [0], pygame.mouse.get_pos() [1])
+                                print(pos) #for checking
+
+                        #ensure there is always a safe way to end the program if the touch screen fails
+                        if event.type == KEYDOWN:
+                                if event.key == K_ESCAPE:
+                                        sys.exit()
+        time.sleep(0.2)
+        pygame.display.update()
+
+
+size = width, height = 320, 240
+screen = pygame.display.set_mode(size)
+main()
